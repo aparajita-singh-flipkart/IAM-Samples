b393ccf62f63a23fe68692e51294640f38a9912c
aparajita-singh-flipkart <108419181+aparajita-singh-flipkart@users.noreply.github.com>
2023-12-03 01:42:02 +0530

    Update get-initial-access-token.sh

--- openid-connect/keycloak/samples/get-initial-access-token.sh
+++ openid-connect/keycloak/samples/get-initial-access-token.sh
@@ -4,3 +4,15 @@ curl --location 'http://localhost:8080/realms/master/protocol/openid-connect/tok
 --data-urlencode 'username=admin' \
 --data-urlencode 'password=password' \
 --data-urlencode 'grant_type=password'
+
+# Response:
+{
+    "access_token": "{{ access_token }}",
+    "expires_in": 60,
+    "refresh_expires_in": 1800,
+    "refresh_token": "{{ refresh_token }}",
+    "token_type": "Bearer",
+    "not-before-policy": 0,
+    "session_state": "c4916af4-0109-41b2-96ae-67fe8e1b262f",
+    "scope": "email profile"
+}
